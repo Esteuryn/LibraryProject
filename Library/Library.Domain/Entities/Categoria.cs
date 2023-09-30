0aecf4d91b0beea8b1c87949f2fdc4b7b18121bc
Engel Castillo Lorenzo <134871631+Experl@users.noreply.github.com>
2023-09-30 01:37:15 -0300

    Cambio #1

---
+++ Library/Library.Domain/Entities/Categoria.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+
+namespace Library.Domain.Entities
+{
+    internal class Categoria
+    {
+        public int IdCategoria { get; set; }
+
+        public string Descripcion {  get; set; }
+
+        public string Estado { get; set; }
+    }
+}
