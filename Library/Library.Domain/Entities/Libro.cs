eddd42126e090795b28e5aa909115cdf19d6b494
Jese75 <20221091@itla.edu.do>
2023-09-30 14:41:56 -0500

    actu 1

---
+++ Library/Library.Domain/Entities/Libro.cs
@@ -0,0 +1,32 @@
+﻿using Library.Domain.Core;
+using System;
+
+
+namespace Library.Domain.Entities
+{
+    public class Libro : BaseEntity
+
+    {
+        public int IdLibro { get; set; }
+
+        public string? Titulo { get; set; }
+
+        public string? Autor { get; set; }
+
+        public int? IdCategoria { get; set; }
+
+        public string? Editorial { get; set; }
+
+        public string? Ubicacion { get; set; }
+
+        public int? Ejemplares { get; set; }
+
+        public byte[]? Portada { get; set; }
+
+
+
+
+
+
+    }
+}
