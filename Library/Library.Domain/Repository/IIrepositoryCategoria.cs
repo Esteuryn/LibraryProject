6c3762fcba3c687fdb91f579c10c7a470b8cd294
Engel Castillo Lorenzo <134871631+Experl@users.noreply.github.com>
2023-09-30 02:24:39 -0300

    Cambio #2

---
+++ Library/Library.Domain/Repository/IIrepositoryCategoria.cs
@@ -0,0 +1,18 @@
+﻿using Library.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Library.Domain.Repository
+{
+    internal interface IIrepositoryCategoria
+    {
+
+        void Save(Categoria categoria);
+
+        void Update(Categoria categoria);
+        void Delete(Categoria categoria);
+
+        List<Categoria> GetCategoria();
+        Categoria GetCategoria(int id);
+        
+    }
+}
