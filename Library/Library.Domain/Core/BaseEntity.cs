b29500bee991532c4853650d9d3386b83e78e5e3
Esteuryn <esteuryn2018@gmail.com>
2023-09-29 23:00:57 -0400

    actualizando 6

---
+++ Library/Library.Domain/Core/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Library.Domain.Core
+{
+    public abstract class BaseEntity
+    {
+        public bool Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string? Codigo { get; set; }
+    }
+}
---
+++ Library/Library.Domain/Core/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Domain.Core
+{
+    public abstract class User : BaseEntity
+    {
+        public string? Correo { get; set; }
+        public string? Clave { get; set; }
+    }
+}
