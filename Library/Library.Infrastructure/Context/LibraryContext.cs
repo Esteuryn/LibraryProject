1737aa7a1b5761a995211efa3f6e76c166c83b6a
Engel Castillo Lorenzo <134871631+Experl@users.noreply.github.com>
2023-10-01 10:00:55 -0300

    Cambio

---
+++ Library/Library.Infrastructure/Context/LibraryContext.cs
@@ -0,0 +1,18 @@
+using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Infrastructure.Context
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> categoria { get; set; }
+    }
+}
