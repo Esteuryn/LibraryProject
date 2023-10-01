e24011740720baebc1f2c96f1d54bc66b068b74d
Jese75 <20221091@itla.edu.do>
2023-09-30 19:27:20 -0500

    actu 4

---
+++ Library/Library.Infrastructure/Context/BibliotecaContext.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Infrastructure.Context
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options);
+
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
---
+++ Library/Library.Infrastructure/Interfaces/IDLibroRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Domain.Entities;
+using Library.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Infrastructure.Interfaces
+{
+    public interface IDLibroRepository : IBaseRepository<Libro>
+    {
+        //metodos exclusivos de la entidad //
+
+    }
+}
