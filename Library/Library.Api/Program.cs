a84190e1eb7940678678d478cb6494ce6bab111a
Bayardo Enrique Ortiz Beltre <20220016@itla.edu.do>
2023-10-24 23:34:54 -0400

    todavia no work

--- Library/Library.Api/Program.cs
+++ Library/Library.Api/Program.cs
@@ -1,10 +1,21 @@
+using Library.Infrastructure.Context;
 using Library.Infrastructure.Interfaces;
 using Library.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+
+// Dependencia del contexto
+
+builder.Services.AddDbContext<BibliotecaContext>(options => options.UseSqlServer(""));
+// Dependencia de los repositorios
+builder.Services.AddTransient<INumeroCorrelativoRepository, NumeroCorrelativoRepository>();
+
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +24,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-builder.Services.AddScoped<INumeroCorrelativoRepository, NumeroCorrelativoRepository>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
