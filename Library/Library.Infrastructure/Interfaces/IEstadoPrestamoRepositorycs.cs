d572a64a5ed37b81cbb77ca01c97744f82a01141
Esteuryn <esteuryn2018@gmail.com>
2023-10-30 19:24:09 -0400

    Metodos parte 2

--- Library/Library.Infrastructure/Interfaces/IEstadoPrestamoRepositorycs.cs
+++ Library/Library.Infrastructure/Interfaces/IEstadoPrestamoRepositorycs.cs
@@ -12,22 +12,13 @@ namespace Library.Infrastructure.Interfaces
 
         public interface IEstadoPrestamo
         {
-            // Método para obtener un estado de préstamo por su ID
-            EstadoPrestamo ObtenerEstadoPrestamoPorId(int id);
-
-            // Método para agregar un nuevo estado de préstamo
-            void AgregarEstadoPrestamo(EstadoPrestamo estado);
-
-            // Método para actualizar un estado de préstamo existente
-            void ActualizarEstadoPrestamo(EstadoPrestamo estado);
-
-            // Método para eliminar un estado de préstamo por su ID
-            void EliminarEstadoPrestamo(int id);
-
-            // Método para obtener todos los estados de préstamo
-            List<EstadoPrestamo> ObtenerTodosLosEstadosPrestamo();
+            bool Exists(Expression<Func<EstadoPrestamo, bool>> filter);
+            List<EstadoPrestamo> GetEntities();
+            EstadoPrestamo GetEntity(int id);
+            void Remove(EstadoPrestamo estadoPrestamo);
+            void Save(EstadoPrestamo estadoPrestamo);
+            void Update(EstadoPrestamo estadoPrestamo);
 
         }
     }
 }
-
