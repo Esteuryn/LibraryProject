97c44a110a0f83ddc83df4240cbceb2bc1b3206c
Jese75 <20221091@itla.edu.do>
2023-11-03 18:08:58 -0500

    Api

---
+++ Library/Library.Api/Controllers/LibroController.cs
@@ -0,0 +1,53 @@
+﻿using Library.Domain.Entities;
+using Library.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly ILibroRepository libroRepository;
+
+        public LibroController(ILibroRepository libroRepository)
+        {
+            this.libroRepository = libroRepository;
+        }
+
+        // GET: api/<LibroController>
+        [HttpGet]
+        public IEnumerable<Libro> Get()
+        {
+           var libros = this.libroRepository.GetLibros();
+            return libros;
+        }
+       
+        // GET api/<LibroController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LibroController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LibroController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LibroController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Library/Library.Api/Program.cs
+++ Library/Library.Api/Program.cs
@@ -1,6 +1,20 @@
+using Library.Domain.Entities;
+using Library.Infrastructure.Context;
+using Library.Infrastructure.Interfaces;
+using Library.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Agregar dependencia del contexto //
+
+builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext")));
+
+//dependencias de los repositorios//
+
+builder.Services.AddTransient<ILibroRepository, LibroRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
