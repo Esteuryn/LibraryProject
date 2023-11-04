a5415920cc76665859520c1c675b0854c7f16679
Esteuryn <esteuryn2018@gmail.com>
2023-11-03 21:04:59 -0400

    Cambios problems con primary Key

--- Library/Library.Api/Program.cs
+++ Library/Library.Api/Program.cs
@@ -1,6 +1,17 @@
+using Library.Infrastructure.Context;
+using Library.Infrastructure.Interfaces;
+using Library.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Agregar dependencia del contexto 
+builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext")));
+
+//Dependencia de los repositorios
+builder.Services.AddTransient<IEstadoPrestamorRepository, EstadoPrestamoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Library/Library.Infrastructure/Interfaces/IEstadoPrestamoRepositorycs.cs
+++ Library/Library.Infrastructure/Interfaces/IEstadoPrestamoRepositorycs.cs
@@ -9,17 +9,12 @@ namespace Library.Infrastructure.Interfaces
 {
     public interface IEstadoPrestamorRepository : IBaseRepository<EstadoPrestamo>
     {
-        // Aqui van los metodos exclusivos de la entidad
-
-        public interface IEstadoPrestamo
-        {
+        // Aqui van los metodos exclusivos de la entidad 
             bool Exists(Expression<Func<EstadoPrestamo, bool>> filter);
             List<EstadoPrestamo> GetEstadoPrestamos();
             EstadoPrestamo GetEstadoPrestamo(int id);
             void Remove(EstadoPrestamo estadoPrestamo);
             void Save(EstadoPrestamo estadoPrestamo);
             void Update(EstadoPrestamo estadoPrestamo);
-
-        }
     }
 }
